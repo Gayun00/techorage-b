d081ee5b456cc4260d0fda70016a005b1f941b76
Gayun00 <gayunlee11@gmail.com>
2023-10-12 19:19:16 +0900

    Chore: rename auth to user

--- src/auth/user.service.ts
+++ src/auth/user.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/ create-user.dto';
 
 @Injectable()
-export class AuthService {
+export class UserService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
   ) {}
+
+  async signIn(authCredentialDto: AuthCredentialsDto) {
+    // const { name, email } = authCredentialDto;
+  }
 }
