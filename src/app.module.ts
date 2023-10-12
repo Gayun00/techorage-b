7221a250c2160ade8cc98ef0853f8e19f81d9aac
Gayun00 <gayunlee11@gmail.com>
2023-10-12 18:37:45 +0900

    Add: Auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { typeORMConfig } from './configs/typeorm.config';
 import { ArticlesModule } from './articles/articles.module';
 import { Module } from '@nestjs/common';
 import { KeywordsModule } from './keywords/keywords.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
     ArticlesModule,
     KeywordsModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
